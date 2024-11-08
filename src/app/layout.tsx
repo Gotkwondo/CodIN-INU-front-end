1445ac500c10047380d15757b48e195afb27a54d
hyunjung <poggo201103@gmail.com>
2024-11-09 02:20:18 +0900

    SC-9 프로젝트 파일 생성

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+import './globals.css';
+import { ReactNode } from 'react';
+import BottomNav from '../components/BottomNav';
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+    return (
+        <html lang="ko">
+        <head>
+            <title>인천대학교 정보대 SNS</title>
+        </head>
+        <body className="pb-16"> {/* 바텀 네비게이션 높이만큼 패딩 추가 */}
+        {children}
+        </body>
+        </html>
+    );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid">
+      대충 SNS CODIN 에 대한 설명하는 홈페이지
+    </div>
+  );
+}
