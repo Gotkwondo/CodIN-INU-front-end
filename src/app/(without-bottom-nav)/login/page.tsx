927d749fa143626be032d2c826436df8e75871fe
yereong <lily030624@gmail.com>
2024-11-18 16:14:07 +0900

    [SC-36] Feat : 로그인 뷰 구현

--- src/app/(without-bottom-nav)/login/page.tsx
+++ src/app/(without-bottom-nav)/login/page.tsx
@@ -1,8 +1,29 @@
-export default function MessagesPage() {
+import './login.css';
+
+export default function LoginPage() {
     return (
-        <div>
-            <h1>쪽지 페이지</h1>
-            {/* 쪽지 기능 구현 */}
+        <div className="Login">
+            <div id="logo"></div>
+            <div id="inputBox">
+                <input id="email" placeholder='이메일'></input>
+                <input id="password" placeholder='비밀번호'></input>
+                <div id='remainCont'>
+                    <input type="checkbox" id="remain"></input>
+                    <label id="remainText">로그인 유지</label>
+                </div>
+          
+                
+            </div>
+            <div id="else">
+                <button id="findId">아이디 찾기 </button>
+                <div id="divider"> | </div>
+                <button id="findPW"> 비밀번호 찾기</button>
+                <div id="divider"> | </div>
+                <button id="signup">회원가입</button>
+            </div>
+            <div id="buttonCont">
+                <button id='loginBtn'>로그인</button>
+            </div>
         </div>
     );
 }
