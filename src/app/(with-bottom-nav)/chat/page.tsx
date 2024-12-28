eac09faa550379e7e795f5bfa6d5dd7fc21a83f6
yereong <lily030624@gmail.com>
2024-12-28 20:39:44 +0900

    [SC-88] feat:채팅리스트 api 연결 완료

--- src/app/(with-bottom-nav)/chat/page.tsx
+++ src/app/(with-bottom-nav)/chat/page.tsx
@@ -1,62 +1,62 @@
 'use client'
 import './chat.css';
 import { useRouter } from 'next/navigation';
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
+import BottomNav from "@/components/BottomNav";
+import { AuthContext } from '@/context/AuthContext';
+import { GetChatRoomData } from '@/api/getChatRoomData';
 
 export default function Chat() {
     const router = useRouter();
+    const authContext = useContext(AuthContext);
     
-    const [email, setEmail] = useState<string>("");
-    const [isMailSent, setMailSent] = useState<boolean>(false);
-    const hadleInputChange = (e:React.ChangeEvent<HTMLInputElement>):void => {
-        setEmail(e.target.value);
-    };
+    if (!authContext) {
+        throw new Error('AuthContext를 사용하려면 AuthProvider로 감싸야 합니다.');
+    }
+
+    const { Auth } = authContext;
+    const [ chatList, setChatList] = useState<any>([]);
+    const [accessToken, setToken] = useState<string>('');
+   
 
 
     interface ChatData {
-        title: string;
-        lastChatMessage: string;
-        lastChatMessageDate: string;
+        chatRoomID : string;
+        roomName: string;
+        message: string;
+        currentMessageDate: string;
+        notificationEnabled : boolean;
     }
 
     interface ChatListProps{
         chatList: ChatData[];
     } 
-   
-    const dummy:ChatData[] = [
-        {title: '게시글 제목',
-            lastChatMessage: '최근 보낸 카톡 내역',
-            lastChatMessageDate:'오후 21:39'
-        },
-        {title: '게시글 제목',
-            lastChatMessage: '최근 보낸 카톡 내역',
-            lastChatMessageDate:'오후 21:39'
-        },
-        {title: '게시글 제목',
-            lastChatMessage: '최근 보낸 카톡 내역',
-            lastChatMessageDate:'오후 21:39'
-        },
-        {title: '게시글 제목',
-            lastChatMessage: '최근 보낸 카톡 내역',
-            lastChatMessageDate:'오후 21:39'
-        },
-        {title: '게시글 제목',
-            lastChatMessage: '최근 보낸 카톡 내역',
-            lastChatMessageDate:'오후 21:39'
-        },
-        {title: '게시글 제목',
-            lastChatMessage: '최근 보낸 카톡 내역',
-            lastChatMessageDate:'오후 21:39'
-        },
-        {title: '게시글 제목',
-            lastChatMessage: '최근 보낸 카톡 내역',
-            lastChatMessageDate:'오후 21:39'
-        },
-        {title: '게시글 제목',
-            lastChatMessage: '최근 보낸 카톡 내역',
-            lastChatMessageDate:'오후 21:39'
+
+    useEffect(() => {
+        const token = localStorage.getItem("accessToken");
+        if (token) {
+            setToken(token);
         }
-    ]
+    },[])
+   
+    useEffect(() => {
+        if (!accessToken) return;
+
+        const getChatRoomData = async()=>{
+        try{        
+            console.log('토큰:', accessToken);
+            const chatRoomData = await GetChatRoomData(accessToken);
+            console.log(chatRoomData.dataList);
+            setChatList(chatRoomData.dataList || []);
+        }catch(error){
+            console.log("채팅방 정보를 불러오지 못했습니다.", error);
+            setChatList([]);
+        }
+    }
+
+    getChatRoomData();
+    }, [accessToken])
+
     const ChatList = ({chatList}: ChatListProps)=>{
         const options: Intl.DateTimeFormatOptions = { month: 'long', day: 'numeric' };
         return(
@@ -66,11 +66,11 @@ export default function Chat() {
                    <div id='chatCont'>
                     <div id="profile"></div>
                     <div id="main_cont">
-                        <div id="name">{data.title}</div>
-                        <div id="ment">{data.lastChatMessage }</div>
+                        <div id="name">{data.roomName}</div>
+                        <div id="ment">{data.message }</div>
                     </div>
                     <div id="ect">
-                        <div id="time">{new Date(data.lastChatMessageDate).toLocaleDateString('ko-KR', options)}</div>
+                        <div id="time">{new Date(data.currentMessageDate).toLocaleDateString('ko-KR', options)}</div>
                        
                     </div>
                 </div> 
@@ -89,8 +89,9 @@ export default function Chat() {
                 <button id='searchBtn'></button>
             </div>
             <div id='tag'>{`<ul>`}</div>
-            <ChatList chatList={dummy}/>
+            <ChatList chatList={chatList}/>
            <div id='tag1'>{`</ul>`}</div>
+           <BottomNav />
         </div>
     );
 }
