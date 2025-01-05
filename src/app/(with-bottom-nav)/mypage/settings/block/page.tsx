30461934600cfa8007793a3547a72536e37fc661
yereong <lily030624@gmail.com>
2025-01-06 00:35:37 +0900

    [SC-89] feat:  빌드성공

--- src/app/(with-bottom-nav)/mypage/settings/block/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function EditProfilePage() {
-    return (
-        <div className="min-h-screen bg-white p-4">
-            <h1 className="text-lg font-bold mb-4">프로필 편집</h1>
-            <p>이 페이지에서 프로필을 편집할 수 있습니다.</p>
-        </div>
-    );
-}
