955973adb0506d604c4639ebba0f3f3e2d9d197d
ParkKyungWan <pkw8117197@gmail.com>
2025-01-30 18:34:41 +0900

    [feat] 타임라인 클릭 시, 해당 강좌 highlight

--- src/app/(auth-required)/roomstatus/roomItem.tsx
+++ src/app/(auth-required)/roomstatus/roomItem.tsx
@@ -9,16 +9,33 @@ import { Lecture } from "./interfaces/page_interface";
 const RoomItem: React.FC<roomItemProps> = ({ RoomName, LectureList, RoomStatusList, BoundaryList }) => {
 
     const [clicked, setClicked] = React.useState<number>(-1);
-    const [activeIndexList, setActiveIndexList] = React.useState<number[]>([]);
+    const [activeIndexList, setActiveIndexList] = React.useState<number[]>(Array.from({ length: 36 }, () => 0));
     const [touchedLecture, setTouchedLecture] = React.useState<Lecture>(null);
 
     const highlightTouchedLecture = (idx: number) =>{
         if(idx >= 0){
-            
+            let st = 0;
+            let end = 36;
+            for(let i = idx ; i >= 0 ; i--){
+                if(BoundaryList[i] === 1) { st = i; break; }
+            }
+            for(let i = idx ; i <= 36 ; i++){
+                if(BoundaryList[i] === 1) { end = i; break; }
+            }
+            let nl = Array.from({ length: 36 }, () => 0);
+            for (let i = st; i <= end; i++) {
+                nl[i] = 1;
+            }
+            setActiveIndexList(nl);
+            console.log(nl);
+        }else{
+            const emptyList = Array.from({ length: 36 }, () => 0);
+            setActiveIndexList(emptyList);
         }
     }
     const onClickTimeLine = (idx: number) =>{
         setClicked(idx);
+        highlightTouchedLecture(idx);
     }
     return (
         <div className="flex flex-col gap-[12px]">
@@ -40,7 +57,7 @@ const RoomItem: React.FC<roomItemProps> = ({ RoomName, LectureList, RoomStatusLi
                             id={`room-${RoomName}-time-${index}`}  
                             onTouchStart={()=>onClickTimeLine(index)}
                             onTouchEnd={()=>onClickTimeLine(-1)}
-                            className={`relative flex-1 h-[12px] ${status ? 'bg-[#0D99FF]' : 'bg-[#EBF0F7]'}`}
+                            className={`relative flex-1 h-[12px] ${activeIndexList[index] ? 'bg-[#212121]' : status ? 'bg-[#0D99FF]' : 'bg-[#EBF0F7]'}`}
                         ><RoomItemDetail isActive={clicked === index}/></button>
                     ))}
                 </div>
