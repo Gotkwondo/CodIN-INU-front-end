92bd772b3ef0ed4561da43c85ec166205fb5719f
오주현 <dhwngus99@gmail.com>
2025-02-05 16:42:34 +0900

    ✨ feat(헤더): 헤더 추가

---
+++ src/app/(auth-required)/main/info/course-reviews/constants.ts
@@ -0,0 +1,6 @@
+export const DEPARTMENTS = [
+  { label: '컴공', value: 'computerScienceEngineering' },
+  { label: '정통', value: ' telecommunicationEngineering' },
+  { label: '임베', value: 'embeddedSystemsEngineering' },
+  { label: '공통', value: 'all'}
+];
---
+++ src/app/(auth-required)/main/info/course-reviews/types.ts
@@ -0,0 +1,4 @@
+export type labelType = {
+  label: string,
+  value: string
+}
