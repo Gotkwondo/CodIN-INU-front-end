7de3052a4fda557e540205e5a2fe0883489d15ab
오주현 <dhwngus99@gmail.com>
2025-02-05 18:39:54 +0900

    ✨ feat(검색바 퍼블리싱): 검색바 퍼블리싱

--- src/app/(auth-required)/main/info/course-reviews/page.tsx
+++ src/app/(auth-required)/main/info/course-reviews/page.tsx
@@ -5,6 +5,7 @@ import Header from '@/components/Layout/header/Header';
 import { DEPARTMENTS } from './constants';
 import { labelType } from './types';
 import { useState } from 'react';
+import { Input } from '@/components/input/Input';
 
 
 
@@ -13,30 +14,40 @@ const courseReviewPage = () => {
   const token = localStorage.getItem("accessToken");
   console.log('dd', token)
   const [selectedDepartment, setSelectedDepartment] = useState<labelType>({ label: '', value: ''});
-  
+  const [searchKeyword, setSearchKeyword] = useState<string>('');
   
   const selectDepartmentHandler = (selectedLabel: string, selectedValue: string) => {
     setSelectedDepartment({ label: selectedLabel, value: selectedValue });
   }
 
   return (
-    <div className="bg-white min-h-screen overflow-x-hidden w-full">
+    <div className="bg-white min-h-screen overflow-x-hidden w-full relative">
       <Header>
         <Header.Title>수강 후기</Header.Title>
         <Header.BackButton />
       </Header>
-      <div className="mt-24">
-        <div className="w-full flex justify-between">
-          {DEPARTMENTS.map(({ label, value }: labelType) => {
-            return (
-              <SmRoundedBtn
-                key={`selectDepartment_${value}`}
-                text={label}
-                onClick={() => selectDepartmentHandler(label, value)}
-                status={value === selectedDepartment.value ? 1 : 0}
-              />
-            );
-          })}
+      <div className="mt-28">
+        <div className="flex flex-col w-full">
+          <div className="w-full flex justify-between ">
+            {DEPARTMENTS.map(({ label, value }: labelType) => {
+              return (
+                <SmRoundedBtn
+                  key={`selectDepartment_${value}`}
+                  text={label}
+                  onClick={() => selectDepartmentHandler(label, value)}
+                  status={value === selectedDepartment.value ? 1 : 0}
+                />
+              );
+            })}
+          </div>
+          <div className="mt-4 w-full">
+            <Input
+              className="w-full"
+              placeholder="과목명, 교수명 입력"
+              value={searchKeyword}
+              onChange={(e) => setSearchKeyword(e.target.value)}
+            />
+          </div>
         </div>
       </div>
     </div>
