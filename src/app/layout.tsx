0bec9782e4e8efbc53a1356c484fcc8e0c2ce385
ParkKyungWan <pkw8117197@gmail.com>
2025-02-06 20:18:32 +0900

    [style] #scrollbar-hidden 속성 추가 : 스크롤바 보여지지 않음 (html,body,defaultBody에 적용)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,13 +6,13 @@ import { AuthProvider } from '@/context/AuthContext';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
     return (
-        <html lang="ko" className="w-full h-full relative">
+        <html id="scrollbar-hidden" lang="ko" className="w-full h-full relative">
             <head>
                 <title>인천대학교 정보대 SNS</title>
             </head>
             <AuthProvider>
             <UserProvider>
-            <body className="bg-slate-800 max-w-full min-h-full relative flex justify-center"> {/* 바텀 네비게이션 높이만큼 패딩 추가 */}
+            <body id="scrollbar-hidden" className="bg-slate-800 max-w-full min-h-full relative flex justify-center"> {/* 바텀 네비게이션 높이만큼 패딩 추가 */}
                 <div className='bg-white w-full min-h-full max-w-[500px] relative'>
                     {children}
                 </div>
