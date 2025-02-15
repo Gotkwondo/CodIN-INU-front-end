3665ac8fd68a3adb20bb8237ed51b4cf1ce2dfa6
오주현 <dhwngus99@gmail.com>
2025-02-15 22:37:10 +0900

    ✨ feat(props 추가): 백업용

--- src/components/common/Review/ReviewComment.tsx
+++ src/components/common/Review/ReviewComment.tsx
@@ -1,17 +1,27 @@
 import Image from 'next/image';
 import { CiHeart } from "react-icons/ci";
 
-const ReviewComment = () => {
+type ReviewCommentType = {
+  starRating?: number;
+  content?: string;
+}
+
+const ReviewComment = ({starRating=0, content='Blank'}: ReviewCommentType) => {
   const isLike = true;
   return (
     <div className="border-t">
-      <div className="flex justify-between items-center">
-        <p>{`${"starRating"}`}</p>
-        {/* <Image src={HeartIcon} alt='좋아요' width={20} height={20} className='fill-red-700' /> */}
-        <CiHeart
-          fill={isLike ? "#0D99FF" : ``}
-          color={isLike ? "#0D99FF" : ``}
-        />
+      <div className="mt-4">
+        <div className="flex justify-between items-center">
+          <p className="text-xl">{`${
+            starRating % 1 ? starRating : starRating + ".0"
+          }`}</p>
+          {/* <Image src={HeartIcon} alt='좋아요' width={20} height={20} className='fill-red-700' /> */}
+          <CiHeart
+            fill={isLike ? "#0D99FF" : ``}
+            color={isLike ? "#0D99FF" : ``}
+            style={{ width: "20px", height: "20px" }}
+          />
+        </div>
       </div>
     </div>
   );
