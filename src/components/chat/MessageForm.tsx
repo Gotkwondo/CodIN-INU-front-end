9d5e3b7fb30fb2311c970cbde7fc314a740e7c99
yereong <lily030624@gmail.com>
2025-03-11 15:49:06 +0900

    [fix] 채팅 롤백

--- src/components/chat/MessageForm.tsx
+++ src/components/chat/MessageForm.tsx
@@ -9,6 +9,7 @@ interface Message {
     createdAt: string;
     contentType: string;
     me: boolean;
+    
 }
 
 interface MessageFormProps {
@@ -26,18 +27,20 @@ const MessageForm = ({ onMessageSubmit, myId, imageFile, setImageFile }: Message
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
 
-        const getCurrentTime = (date: Date) => {
+        const getCurrentTime = (date:Date) => {
             const year = date.getFullYear();
-            const month = (date.getMonth() + 1).toString().padStart(2, '0');
+            const month = (date.getMonth() + 1).toString().padStart(2, '0'); // 월은 0부터 시작하므로 1을 더해야 함
             const day = date.getDate().toString().padStart(2, '0');
             const hours = date.getHours().toString().padStart(2, '0');
             const minutes = date.getMinutes().toString().padStart(2, '0');
             const seconds = date.getSeconds().toString().padStart(2, '0');
-
+        
             return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
         };
         const currentTime = new Date();
-        const formattedTime = getCurrentTime(currentTime);
+
+        const formattedTime = getCurrentTime(currentTime)
+
 
         const message: Message = {
             content: messageContent,
@@ -46,6 +49,7 @@ const MessageForm = ({ onMessageSubmit, myId, imageFile, setImageFile }: Message
             id: '',
             createdAt: formattedTime,
             contentType: 'TEXT',
+        
         };
 
         if (imageFile) {
@@ -59,75 +63,67 @@ const MessageForm = ({ onMessageSubmit, myId, imageFile, setImageFile }: Message
                 };
                 onMessageSubmit(imageMessage); // 이미지 메시지 전송
                 setImageFile(null); // 이미지 파일 상태 초기화
-                setImagePreview(null); // 미리보기 초기화
+                setImagePreview(null);
             };
             reader.readAsDataURL(imageFile);
         } else {
             onMessageSubmit(message); // 일반 텍스트 메시지 전송
         }
-        setMessageContent(''); // 메시지 입력 필드 초기화
+        setMessageContent('');
     };
 
     const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files) {
-            const file = e.target.files[0];
-            setImageFile(file); // 선택한 파일 상태 업데이트
-            console.log('이미지 선택됨');
-            // Base64 변환하여 미리보기 설정
-            const reader = new FileReader();
-            reader.onloadend = () => {
-                setImagePreview(reader.result as string);
-            };
-            reader.readAsDataURL(file);
-        }
-    };
+          const file = e.target.files[0]; 
+          setImageFile(file); // 선택한 파일 상태 업데이트
+          console.log('이미지 선택됨');
+          // Base64 변환하여 미리보기 설정
+          const reader = new FileReader();
+          reader.onloadend = () => {
+              setImagePreview(reader.result as string);
+          };
+          reader.readAsDataURL(file);
+        }    
+      };
+
 
     return (
         <div id='imagePrevCont'>
-            {imagePreview && (
-                <div className="image-preview">
-                    <button
-                        id='imgPrevDelete'
-                        onClick={() => {
-                            setImagePreview(null);
-                            setImageFile(null); // 이미지를 삭제할 때 상태 초기화
-                        }}
-                    >
-                        x
-                    </button>
-                    <img id='imagePrev' src={imagePreview} alt="미리보기 이미지" />
-                </div>
-            )}
-            <div id='inputCont'>
-                <input
-                    type="file"
-                    accept="image/*"
-                    onChange={handleImageUpload}
-                    style={{ display: 'none' }}
-                    ref={inputRef}
-                    disabled={!!imageFile} // 이미지가 이미 선택되면 파일 선택 불가
-                />
-                <button
-                    id="imageSubmit"
-                    onClick={() => inputRef.current?.click()} // 이미지 파일 선택
-                    disabled={!!imageFile} // 이미지가 이미 선택되면 버튼 비활성화
-                >
-                    이미지 선택
-                </button>
-                <form onSubmit={handleSubmit} id='messagesendForm' autoComplete='off'>
-                    <input
-                        id='messageInput'
-                        type="text"
-                        value={messageContent}
-                        onChange={(e) => setMessageContent(e.target.value)}
-                        placeholder="메시지를 입력하세요"
-                        autoFocus
-                    />
-                    <button type="submit" id='sendBtn'></button>
-                </form>
-            </div>
-        </div>
-    );
+        {imagePreview && (   
+<div className="image-preview">
+    <button id='imgPrevDelete' onClick={() => {setImagePreview(null); setImageFile(null)}}>x</button> {/* 미리보기 제거 버튼 */}
+ <img id='imagePrev' src={imagePreview} alt="미리보기 이미지" />
+ </div>
+)}
+<div id='inputCont'>
+
+     <input
+type="file"
+accept="image/*"
+onChange={handleImageUpload}
+style={{ display: 'none' }}
+ref={inputRef}
+/>
+<button
+id="imageSubmit"
+onClick={() => inputRef.current?.click()} // 이미지 파일 선택
+></button>
+<form onSubmit={handleSubmit} id='messagesendForm' autoComplete='off'>
+    <input
+        id='messageInput'
+        type="text"
+        value={messageContent}
+        onChange={(e) => setMessageContent(e.target.value)}
+        placeholder="메시지를 입력하세요"
+        autoFocus
+       
+    />
+    <button type="submit" id='sendBtn'></button>
+</form>
+</div>
+</div>
+);
+
 };
 
 export default MessageForm;
