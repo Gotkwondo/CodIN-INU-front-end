f8069602230833ef406270c7fb9ee931ab3e3ff0
yereong <lily030624@gmail.com>
2025-03-13 01:37:33 +0900

    [fix] 채팅 로직 수정

--- src/api/chat/postChatRoom.tsx
+++ src/api/chat/postChatRoom.tsx
@@ -6,6 +6,7 @@ const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 export const PostChatRoom = async (
   roomName: string,
   receiverId: string,
+  referenceId : string,
   retryCount=0
 ): Promise<any> => {
   console.log("전송 데이터", roomName, receiverId);
@@ -17,6 +18,7 @@ export const PostChatRoom = async (
       {
         roomName: roomName,
         receiverId: receiverId,
+        referenceId: referenceId
       }
     );
 
@@ -38,7 +40,7 @@ export const PostChatRoom = async (
                 console.log(res);
                 
                 // PostReissue가 성공한 후에 GetChatRoomData 실행
-                return await PostChatRoom(roomName,receiverId, retryCount + 1); // 재요청
+                return await PostChatRoom(roomName,receiverId,referenceId, retryCount + 1); // 재요청
             } catch (error) {
                 console.error("❌ 토큰 재발급 실패", error);
                 // 토큰 재발급 실패시 더 이상 재시도하지 않고 로그인 페이지로 이동
@@ -48,7 +50,11 @@ export const PostChatRoom = async (
     
         console.error("❌ 2번 재시도 후에도 실패. 로그인 페이지로 이동합니다.");
         window.location.href = "/login";
-    }
+      }else if(status === 403) {
+        const id = data.message.split("/")[1];
+        window.location.href = `/chat/${id}`;
+
+      }
     } else if (error.request) {
       console.error("No response received:", error.request);
     } else {
@@ -64,9 +70,9 @@ export const PostChatRoom = async (
  * @param {string} title - 채팅방 제목
  * @param {string} userId - 상대방 사용자 ID
  */
-export const startChat = async (title, userId) => {
+export const startChat = async (title, userId, referenceId) => {
   try {
-    const response = await PostChatRoom(title, userId);
+    const response = await PostChatRoom(title, userId, referenceId);
     console.log("채팅방 생성이 완료되었습니다", response);
 
     if (response?.data.data.chatRoomId) {
