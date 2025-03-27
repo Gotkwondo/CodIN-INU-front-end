cc9d5b3f23f93ae4f885266b69ee1a83d9ef2672
hyunjung <poggo201103@gmail.com>
2025-03-27 17:53:38 +0900

    빌드 버그 수정 및 마이페이지 수정

--- src/app/(auth-required)/mypage/page.tsx
+++ src/app/(auth-required)/mypage/page.tsx
@@ -1,25 +1,24 @@
 "use client";
-import { useState, useEffect, Suspense } from "react";
+import { useState, useEffect, Suspense, useRef } from "react";
 import Link from "next/link";
 import BottomNav from "@/components/Layout/BottomNav/BottomNav";
 import Header from "@/components/Layout/header/Header";
 import DefaultBody from "@/components/Layout/Body/defaultBody";
 import { PostLogout } from "@/api/user/postLogout";
 import { DeleteUser } from "@/api/user/deleteUser";
+import WebModal, { WebModalHandles } from "@/components/modals/WebModal";
 
 export default function MyPage() {
-  const [userData, setUserData] = useState(null);
+  const [userData, setUserData] = useState<any>(null);
   const [isLoading, setIsLoading] = useState(true);
+  const modalRef = useRef<WebModalHandles>(null);
 
-  // 유저 정보를 가져오는 함수
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-
         const response = await fetch("https://codin.inu.ac.kr/api/users", {
           method: "GET",
           headers: {
-
             "Content-Type": "application/json",
           },
         });
@@ -38,7 +37,8 @@ export default function MyPage() {
 
     fetchUserData();
   }, []);
-  const handleLogout = async (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  const handleLogout = async (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
     try {
       if (confirm("로그아웃 할까요?")) {
@@ -48,15 +48,14 @@ export default function MyPage() {
         alert("성공적으로 로그아웃하였습니다.");
         window.location.href = "/login";
       }
-    } catch (error) {
+    } catch (error: any) {
       console.error("로그아웃 실패", error);
-      const message = error.response.data.message;
+      const message = error.response?.data?.message || "알 수 없는 오류 발생";
       alert(message);
     }
   };
-  const handleDeleteAccount = async (
-    e: React.MouseEvent<HTMLButtonElement>
-  ) => {
+
+  const handleDeleteAccount = async (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
     try {
       if (confirm("정말로 탈퇴하시겠어요?")) {
@@ -72,94 +71,91 @@ export default function MyPage() {
 
         window.location.href = "/login";
       }
-    } catch (error) {
+    } catch (error: any) {
       console.error("탈퇴 실패", error);
-      const message = error.response.data.message;
+      const message = error.response?.data?.message || "알 수 없는 오류 발생";
       alert(message);
     }
   };
+
   const menuItems = [
     { label: "프로필 편집", href: "/mypage/edit" },
     { label: "게시글", href: "/mypage/board/posts" },
     { label: "좋아요", href: "/mypage/board/likes", isSpacer: true },
     { label: "스크랩", href: "/mypage/board/scraps", isSpacer: true },
-    // { label: "알림 설정", href: "/mypage/notifications" },
-    // { label: "차단 관리", href: "/mypage/settings/block", isSpacer: true },
-    { label: "문의하기", href: "https://docs.google.com/forms/d/1pDj4qKQMMVY87zrT-1QvoqljuJap5cDQaFaDeJII00A/edit?pli=1", isSpacer: true },
-    { label: "이용약관", href: "https://sites.google.com/view/codin-privacy-policy/%ED%99%88", isSpacer: true },
+    {
+      label: "문의하기",
+      href: "https://docs.google.com/forms/d/1pDj4qKQMMVY87zrT-1QvoqljuJap5cDQaFaDeJII00A/edit?pli=1",
+      isSpacer: true,
+      useModal: true,
+    },
+    {
+      label: "이용약관",
+      href: "https://sites.google.com/view/codin-privacy-policy/%ED%99%88",
+      isSpacer: true,
+      useModal: true,
+    },
     { label: "로그아웃", onclick: handleLogout },
     { label: "회원 탈퇴", onclick: handleDeleteAccount },
   ];
 
   if (isLoading) {
     return (
-      <div className="min-h-screen flex items-center justify-center">
-        Loading...
-      </div>
+        <div className="min-h-screen flex items-center justify-center">Loading...</div>
     );
   }
 
   if (!userData) {
     return (
-      <div className="min-h-screen flex items-center justify-center">
-        <p>유저 정보를 불러올 수 없습니다.</p>
-      </div>
+        <div className="min-h-screen flex items-center justify-center">
+          <p>유저 정보를 불러올 수 없습니다.</p>
+        </div>
     );
   }
 
-  const navigateToMain = () => {
-    console.log("메인 페이지로 이동");
-  };
-
-  const handleMenuClick = (menuName: string) => {
-    alert(`선택: ${menuName}`);
-  };
   return (
-    <Suspense>
-      <Header>
-        <Header.Title>마이페이지</Header.Title>
-      </Header>
+      <Suspense>
+        <Header>
+          <Header.Title>마이페이지</Header.Title>
+        </Header>
 
-      <DefaultBody hasHeader={1}>
-        {/* 사용자 정보 섹션 */}
-        <div className="flex items-center justify-between pt-[18px]">
-          {/* 프로필 이미지 */}
-          <div className="flex items-center space-x-[12px]">
-            <img
-                className="rounded-full w-[49px] h-[49px] object-cover"
-                src={userData.profileImageUrl}
-                width={49}
-                height={49}
-                alt="프로필 이미지"
-            />
-            <div>
-              {/* 사용자 이름 */}
-              <h2 className="text-main text-Mm">{userData.name}</h2>
-              {/* 이메일 */}
-              <p className="text-sub text-sr">{userData.email}</p>
+        <DefaultBody hasHeader={1}>
+          <div className="flex items-center justify-between pt-[18px]">
+            <div className="flex items-center space-x-[12px]">
+              <img
+                  className="rounded-full w-[49px] h-[49px] object-cover"
+                  src={userData.profileImageUrl}
+                  width={49}
+                  height={49}
+                  alt="프로필 이미지"
+              />
+              <div>
+                <h2 className="text-main text-Mm">{userData.name}</h2>
+                <p className="text-sub text-sr">{userData.email}</p>
+              </div>
             </div>
+            <p className="text-Mm text-[#fff] bg-main px-[8px] py-[2px] rounded-[50px] flex justify-center items-center">
+              인증됨
+            </p>
           </div>
-          <p className="text-Mm text-[#fff] bg-main px-[8px] py-[2px] rounded-[50px] flex justify-center items-center">
-            인증됨
-          </p>
-        </div>
 
-        {/* 메뉴 리스트 */}
-        <ul className="mt-[45px] text-Mm">
-          {menuItems.map((item, index) => (
-              <li
-                  key={index}
-                  className={`flex justify-between items-center w-full px-4 py-3 rounded-lg cursor-pointer ${
-                      item.isSpacer ? "mb-[48px]" : "mb-[24px]"
-                  }`}
-                  onClick={(e) => {
-                    if (item.onclick) {
-                      item.onclick(e);
-                    } else {
-                      window.location.href = item.href;
-                    }
-                  }}
-              >
+          <ul className="mt-[45px] text-Mm">
+            {menuItems.map((item, index) => (
+                <li
+                    key={index}
+                    className={`flex justify-between items-center w-full px-4 py-3 rounded-lg cursor-pointer ${
+                        item.isSpacer ? "mb-[48px]" : "mb-[24px]"
+                    }`}
+                    onClick={(e) => {
+                      if (item.onclick) {
+                        item.onclick(e);
+                      } else if (item.useModal) {
+                        modalRef.current?.openModal(item.href);
+                      } else {
+                        window.location.href = item.href;
+                      }
+                    }}
+                >
               <span
                   className={`${
                       item.label === "프로필 편집" ? "text-active" : "text-main"
@@ -167,14 +163,14 @@ export default function MyPage() {
               >
                 {item.label}
               </span>
-                <img src="/icons/mypage/rightArrow.svg" width={20} height={20} />
-              </li>
-          ))}
-        </ul>
+                  <img src="/icons/mypage/rightArrow.svg" width={20} height={20} />
+                </li>
+            ))}
+          </ul>
+        </DefaultBody>
 
-      </DefaultBody>
-
-      <BottomNav activeIndex={3} />
-    </Suspense>
+        <BottomNav activeIndex={3} />
+        <WebModal ref={modalRef} />
+      </Suspense>
   );
 }
